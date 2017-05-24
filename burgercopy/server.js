f3c66b7095c51088a9d449c1e6cdde714417ca78
Boubacar Balde <user@users-mbp.bmcc.cuny.edu>
2017-05-24 13:23:47 -0400

    deploying to heroku

--- burgercopy/server.js
+++ burgercopy/server.js
@@ -1,8 +1,6 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var methodOverride = require("method-override");
-// Requiring our models for syncing
-//var db = require("./models");
 
 var PORT = process.env.PORT || 3000;
 
